099804d5ca96828cb86029ced92140067066db52
Shahid Shafi <49037894+Shahid-135@users.noreply.github.com>
2026-04-18 03:25:32 +0530

    Delete data directory

--- data/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sample_dataset.dataset import SampleDataset
-from .medic_disaster_dataset.dataset import MedicDisasterDataset
-from .crisis_mmd_dataset.dataset import CrisisMMDDataset
-from .tseqd_dataset.dataset import TSEQDDataset
